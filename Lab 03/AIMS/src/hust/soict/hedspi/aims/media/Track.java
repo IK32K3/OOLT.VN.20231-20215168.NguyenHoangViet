1e51513656aae478b7063c9d2bb158f26336bca7
IK32K3 <hoangviet232003@gmail.com>
2023-11-26 21:34:49 +0700

    Lab 03

---
+++ Lab 03/AIMS/src/hust/soict/hedspi/aims/media/Track.java
@@ -0,0 +1,36 @@
+package hust.soict.hedspi.aims.media;
+
+public class Track implements Playable{
+	private String title;
+	private int length;
+	
+	public String getTitle() {
+		return title;
+	}
+
+
+	public int getLength() {
+		return length;
+	}
+
+	
+	public Track(String title, int length) {
+		super();
+		this.title = title;
+		this.length = length;
+	}
+
+	public Track() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void play() {
+		System.out.println("Playing track: " + this.getTitle());
+		System.out.println("track length: " + this.getLength());
+	}
+	
+	public boolean equals(Track a) {
+		if(this.title.compareTo(a.title) == 0 && this.length == a.length) return true;
+		return false;
+	}
+}
