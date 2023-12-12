9b58d2c528d0e15eac2c9b2758337c3167e19f04
IK32K3 <146173061+IK32K3@users.noreply.github.com>
2023-12-12 21:12:10 +0700

    Update AddCDToStoreScreen.java

--- Lab 04/AIMS/src/hust/soict/hedspi/aims/screen/manager/AddCDToStoreScreen.java
+++ Lab 04/AIMS/src/hust/soict/hedspi/aims/screen/manager/AddCDToStoreScreen.java
@@ -1,5 +1,28 @@
 package hust.soict.hedspi.aims.screen.manager;
 
+import hust.soict.hedspi.aims.Store;
+import hust.soict.hedspi.aims.media.*;
+
 public class AddCDToStoreScreen extends AddItemToStoreScreen  {
 
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void add() {
+		String title = super.getTfTitle().getText();
+		float cost = Float.parseFloat(super.getTfCost().getText());
+		CompactDisc cd = new CompactDisc(title, cost);
+		Store.all.addMedia(cd);
+	}
+	
+	public AddCDToStoreScreen() {
+		super();
+		setTitle("Add CD");
+		// TODO Auto-generated constructor stub
+	}
+	
 }
