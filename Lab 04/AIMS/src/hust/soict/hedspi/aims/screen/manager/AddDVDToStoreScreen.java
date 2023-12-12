599d9ee04b09cd8a3c3ed39e205b763fba2e00f3
IK32K3 <146173061+IK32K3@users.noreply.github.com>
2023-12-12 21:11:54 +0700

    Update AddDVDToStoreScreen.java

--- Lab 04/AIMS/src/hust/soict/hedspi/aims/screen/manager/AddDVDToStoreScreen.java
+++ Lab 04/AIMS/src/hust/soict/hedspi/aims/screen/manager/AddDVDToStoreScreen.java
@@ -1,5 +1,26 @@
 package hust.soict.hedspi.aims.screen.manager;
 
-public class AddDVDToStoreScreen extends AddItemToStoreScreen  {
+import hust.soict.hedspi.aims.Store;
+import hust.soict.hedspi.aims.media.*;
 
+public class AddDVDToStoreScreen extends AddItemToStoreScreen  {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void add() {
+		String title = super.getTfTitle().getText();
+		float cost = Float.parseFloat(super.getTfCost().getText());
+		DigitalVideoDisc dvd = new DigitalVideoDisc(title, cost);
+		Store.all.addMedia(dvd);
+	}
+	
+	public AddDVDToStoreScreen() {
+		super();
+		setTitle("Add DVD");
+		// TODO Auto-generated constructor stub
+	}
+	
 }
