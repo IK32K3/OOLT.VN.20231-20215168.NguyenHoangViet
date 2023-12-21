e8e5b099786d7984e4dca4e3f0a91ef6e7423847
IK32K3 <hoangviet232003@gmail.com>
2023-12-21 21:55:17 +0700

    Lab 02

--- Lab 02/AIMS/src/Aims.java
+++ Lab 02/AIMS/src/Aims.java
@@ -5,7 +5,7 @@ public class Aims {
 		// TODO Auto-generated method stub
 		Cart anOrder = new Cart();
 		
-		/*DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
 		anOrder.addDigitalVideoDisc(dvd1);
 		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
@@ -13,9 +13,9 @@ public class Aims {
 		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		anOrder.addDigitalVideoDisc(dvd3);
-		*/
-		System.out.print("Total cost is: ");
-		System.out.println(anOrder.size());
+		
+		anOrder.removeDigitalVideoDisc(dvd1);
+		anOrder.c_show();
 	}
 
 }
